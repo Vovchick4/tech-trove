088c3e3ff81ecae325eb87316b6e2b48fbc6b8c0
Vovchick4 <vovaspodpivas@gmail.com>
2023-08-05 14:26:21 +0300

    hot-fix: button prelina styling darkmode

--- src/app/api/(http)/products/route.ts
+++ src/app/api/(http)/products/route.ts
@@ -7,7 +7,7 @@ const product = new Product();
 export async function GET(request: NextRequest) {
     try {
         const getSlug = request.nextUrl.searchParams.get("slug");
-        if (getSlug) {
+        if (typeof getSlug === "string") {
             return NextResponse.json(await product.findBySlug(getSlug));
         }
         return NextResponse.json(await product.findAll());
