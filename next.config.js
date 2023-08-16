439ddb96a2fca374859542b39976c6d6fb11118f
Vovchick4 <vovaspodpivas@gmail.com>
2023-08-16 18:07:22 +0300

    feat next output conf

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     distDir: 'dist',
     images: {
         domains: ['images.unsplash.com', 'www.google.com', 'i0.wp.com'],
